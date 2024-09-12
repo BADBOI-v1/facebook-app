b3be88bf6adc9e8ea5ccfe59da976bfec4a1db6d
BADBOI HACKER <165565859+BADBOI-v1@users.noreply.github.com>
2024-09-12 22:43:34 +0100

    Create post.js

---
+++ models/post.js
@@ -0,0 +1,10 @@
+```javascript
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    content: { type: String, required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Post', PostSchema);
+```
